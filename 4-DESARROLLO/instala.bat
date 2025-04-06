30a80085fcf48a8608893d0369a645ca059c6ddc
jfegasu <jfegasu@gmail.com>
2025-04-06 09:49:51 -0500

    .

--- 4-DESARROLLO/instala.bat
+++ 4-DESARROLLO/instala.bat
@@ -1,8 +1,18 @@
+CLS
+@echo off
+@echo INSTALANDO EL ENTORNO VIRTUAL
 pip install virtualenv
+@echo CREANDO EL ENTORNO VIRTUAL
 python -m venv venv
-cd Scripts
-activate
-cd ..
+@echo ACTIVANDO EL ENTORNO VIRTUAL
 
+venv\Scripts\activate
+@echo INSTALANDO DJANGO
 python -m pip install Django==5.2
 python.exe -m pip install --upgrade pip
+@echo INSTALANDO REQUERIMIENTOS
+pip install -r requirements.txt
+call venv\Scripts\deactivate
+
+
+@echo PROCESO DE INSTALACION FINALIZADO
